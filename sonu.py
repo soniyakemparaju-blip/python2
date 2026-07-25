af62986a8e5730898ea2f91c99ab625fbbb5d6e1
soniya c k <soniyakemparaju@gmail.com>
2026-07-25 00:01:03 -0700

    fc

---
+++ sonu.py
@@ -0,0 +1,16 @@
+
+
+# Get student details from the user
+name = input("Enter Student Name: ")
+roll_no = input("Enter Roll Number: ")
+age = int(input("Enter Age: "))
+course = input("Enter Course: ")
+marks = float(input("Enter Marks: "))
+
+
+print("\n----- Student Details -----")
+print("Student Name :", name)
+print("Roll Number  :", roll_no)
+print("Age          :", age)
+print("Course       :", course)
+print("Marks        :", marks)
